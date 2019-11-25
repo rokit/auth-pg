d7529740ae3031347d7e38ce4ae8d78cb36939d4
luke <goodvibrato@gmail.com>
2019-11-25 15:09:41 -0600

    add frontend google signin

--- react-ui/src/pages/SignupForm.js
+++ react-ui/src/pages/SignupForm.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./SignupForm.css";
 
 function SignupForm() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (!window.gapi) return;
+    window.gapi.signin2.render("g-signin2", {
+      scope: "https://www.googleapis.com/auth/plus.login",
+      width: 200,
+      height: 50,
+      longtitle: true,
+      theme: "dark",
+      onsuccess: onGoogleSignIn
+    });
+  }, []);
+
+  const onGoogleSignIn = async user => {
+    console.log("user", user);
+  };
 
   const onChange = e => {
     let inputName = e.target.name;
@@ -29,50 +45,53 @@ function SignupForm() {
     let data = {
       email,
       username,
-      pw: password,
+      pw: password
     };
     let res = await fetch(`/story/add-user`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     });
     if (!res) return;
 
     let res_json = await res.json();
     if (!res_json) return;
-    console.log('res_json', res_json);
+    console.log("res_json", res_json);
   };
 
   return (
-    <form className="signup-form" onSubmit={onSubmit}>
-      <div>
-        <label>Email:</label>
-        <input type="text" name="email" value={email} onChange={onChange} />
-      </div>
-      <div>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <input type="submit" value="Submit" />
-      </div>
-    </form>
+    <>
+      <form className="signup-form" onSubmit={onSubmit}>
+        <div>
+          <label>Email:</label>
+          <input type="text" name="email" value={email} onChange={onChange} />
+        </div>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+      <div id="g-signin2"></div>
+    </>
   );
 }
 
